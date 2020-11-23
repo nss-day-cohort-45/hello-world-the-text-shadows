c29190f9131a02354c0ba8381f2dbc207bdb8ac4
anna-moore <ammoore434@gmail.com>
2020-11-23 13:59:27 -0600

    peru/main.js and script tag

--- script/Peru/main.js
+++ script/Peru/main.js
@@ -1,1 +1,9 @@
 //
+
+import { CityList }  from './cities/cityList'
+import { CitizenList } from './citizens/citizenList'
+import { LandmarkList } from './landmarks/landmarkList'
+
+CityList()
+LandmarkList()
+CitizenList()
