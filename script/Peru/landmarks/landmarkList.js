91801dac5cff755c199f877da11849b37a76d674
anna-moore <ammoore434@gmail.com>
2020-11-23 13:36:45 -0600

    main.js file

--- script/Peru/landmarks/landmarkList.js
+++ script/Peru/landmarks/landmarkList.js
@@ -5,7 +5,7 @@ import { useLandmark } from './dataProvider.js'
 
 export const LandmarkList = () =>{
     //get a reference to the <> element
-    const contentElement = document.querySelector(".landmark")
+    const contentElement = document.querySelector(".peruHTML/landmark")
     //grab a copy on the city list
     const cities = useLandmark()
     //iterates through city collection, translate objects to HTML, places HTML on the DOM
